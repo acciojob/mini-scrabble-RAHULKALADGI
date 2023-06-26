216cb99882d6204697de26e372b37ff9ef48e896
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-06-26 19:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+
+const text = document.getElementById("evaluatedText");
+const h3 = document.querySelector("h3");
+text.addEventListener("keyup",func);
+function func() {
+	h3.innerText = text.value.length;
+}
+
